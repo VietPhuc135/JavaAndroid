86e04d4959a3135f2cdc6b6db2efcf1ce7bba4fc
DoThanhh83 <100203762+DoThanhh83@users.noreply.github.com>
2023-12-17 21:10:30 +0700

    lamba code

--- app/src/main/java/com/example/impactjava/Lambda.java
+++ app/src/main/java/com/example/impactjava/Lambda.java
@@ -1,9 +1,13 @@
 package com.example.impactjava;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Lambda extends AppCompatActivity {
 
     Button btnIterator, btnForeach, btnLambda, btnStream;
+    volatile boolean isRunning = false;
+    int gcCount;
+    int sum, suml;
+
+    void helper(Runnable action) {
+        action.run();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +63,91 @@ public class Lambda extends AppCompatActivity {
                 startActivity(intent3);
             }
         });
+        Button startButton = findViewById(R.id.btnStart3);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startExecution();
+            }
+        });
+
+        Button stopButton = findViewById(R.id.btnStop3);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopExecution();
+            }
+        });
+
 
         Toast.makeText(Lambda.this, "Tab Lambda", Toast.LENGTH_LONG).show();
+
+    }
+    private void stopExecution() {
+        isRunning = false;
+    }
+    @SuppressLint("SetTextI18n")
+    private void startExecution() {
+        isRunning = true;
+
+        RadioGroup radioGroupLamba = (RadioGroup) findViewById(R.id.radioGroup3);
+        int idCheckedLamba = radioGroupLamba.getCheckedRadioButtonId();
+        switch (idCheckedLamba) {
+            case R.id.radioButton3:
+                new Thread(() -> {
+                    // Bắt đầu tính thời gian
+                    long startTime = System.currentTimeMillis();
+
+                    while (isRunning) {
+                        helper(() -> suml++);
+                    }
+
+                    // Kết thúc tính thời gian
+                    long endTime = System.currentTimeMillis();
+                    long duration = endTime - startTime; // Thời gian chạy câu lệnh trong mili giây
+
+                    runOnUiThread(() -> {
+                        System.out.println("Tổng: " + suml);
+                        System.out.println("Thời gian chạy: " + duration + " msec");
+
+                        // Hiển thị thời gian chạy trên Log
+                        Log.d("Duration", "Duration: " + duration + " msec");
+                        Toast.makeText(Lambda.this, "Duration: " + duration + " msec", Toast.LENGTH_LONG).show();
+
+                    });
+                    TextView txt1 = (TextView) findViewById(R.id.txtElapsed3);
+                    txt1.setText("Elapsed time (msec): " + duration);
+                }).start();
+
+            case R.id.radioButton32:
+                Runnable action = () -> sum++;
+
+                new Thread(() -> {
+                    // Bắt đầu tính thời gian
+                    long startTime = System.currentTimeMillis();
+
+                    while (isRunning) {
+                        helper(action);
+                    }
+
+                    // Kết thúc tính thời gian
+                    long endTime = System.currentTimeMillis();
+                    long duration = endTime - startTime; // Thời gian chạy câu lệnh trong mili giây
+
+                    runOnUiThread(() -> {
+                        System.out.println("Tổng: " + sum);
+                        System.out.println("Thời gian chạy: " + duration + " msec");
+
+                        // Hiển thị thời gian chạy trên Log
+                        Log.d("Duration", "Duration: " + duration + " msec");
+                        Toast.makeText(Lambda.this, "Duration: " + duration + " msec", Toast.LENGTH_LONG).show();
+
+                    });
+                    TextView txt1 = (TextView) findViewById(R.id.txtElapsed32);
+                    txt1.setText("Elapsed time (msec): " + duration);
+                    TextView txtGC = (TextView) findViewById(R.id.txtNoGC3);
+                    txtGC.setText("No. of GCs: " + gcCount);
+                }).start();
+        }
     }
 }
